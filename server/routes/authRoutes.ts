e00c06e860a763dc01159499ad979e977cda6533
Isaaaya <snizhanna.bushko@gmail.com>
2024-07-22 11:06:53 +0300

    Remove unused routes

--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {signIn, signUp, signOut} = require('../controllers/authController');
 const {isAuthenticated} = require('../middleware/authMiddleware');
-// const validateMiddleware = require('../middleware/validateMiddleware');
-// const {validate} = require('../models/user');
 
 router.post('/sign-in', signIn);
 router.post('/sign-up', signUp);
