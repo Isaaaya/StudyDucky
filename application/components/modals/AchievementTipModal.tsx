bc6e509a211ca4403c8b245232df3f1b45891cad
Isaaaya <snizhanna.bushko@gmail.com>
2024-07-24 10:14:18 +0300

    Change styling

--- application/components/modals/AchievementTipModal.tsx
+++ application/components/modals/AchievementTipModal.tsx
@@ -6,11 +6,11 @@ const AchievementTipModal = ({visible, tooltip, setTooltipVisible}: {visible: bo
   return (
     <Modal visible={visible} transparent>
         <View className='w-full h-full bg-black/30'>
-            <View className='flex items-center w-[80%] m-auto bg-white p-5 rounded-xl border-4 border-blue-400 space-y-4'>
+            <View className='flex items-center w-[80%] m-auto bg-white p-5 rounded-xl border-4 border-blue-400'>
                 <Text className='text-xl font-semibold text-blue-500'>
                     {tooltip}
                 </Text>
-                <ThemedTouchableOpacity caption='Got it' onPress={() => setTooltipVisible(false)} customStyles='py-2 rounded-lg bg-sky-600 px-7' textStyles='text-2xl font-semibold text-white' />
+                <ThemedTouchableOpacity caption='Got it' onPress={() => setTooltipVisible(false)} customStyles='py-2 rounded-lg bg-sky-600 px-7 mt-3' textStyles='text-xl font-semibold text-white' />
             </View>
         </View>
     </Modal>
